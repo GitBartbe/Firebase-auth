5f3c7d15e058bd995d37b5c60d9257f35cc8e0f3
GitBartbe <bartosz.bednrski@wp.pl>
2022-01-17 08:49:05 +0100

    first commit

---
+++ src/components/SignUP/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FormInput } from '../FormInput/Form.input'
+
+export const SignUp = () => {
+    return (
+        <div>
+            <FormInput />
+            
+        </div>
+    )
+}
